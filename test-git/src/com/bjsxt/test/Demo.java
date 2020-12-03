0a2ef75782b3b9515d98d9e0c3c9c2031943b3d6
QiSenMu <347316612@qq.com>
2020-12-03 10:51:24 +0800

    ss

--- test-git/src/com/bjsxt/test/Demo.java
+++ test-git/src/com/bjsxt/test/Demo.java
@@ -2,9 +2,6 @@ package com.bjsxt.test;
 
 public class Demo {
 public static void main(String[] args) {
-	int i=0;
-	System.out.println(i);
-	System.out.println("hello world");
-	int j=1;
+	int a=0;
 }
 }
